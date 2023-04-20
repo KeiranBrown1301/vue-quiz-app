449d6755772a710c1bbfde4ef40e90400e7df9aa
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-20 10:46:32 +0900

    html and styling skeleton added. Front page and one card.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,77 @@
 <template>
-  <h1>Hello there.</h1>
+  <div class="container">
+    <header>
+      <h1>Quizzes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+    <div class="options-container">
+      <div class="card">
+        <img
+          src="https://www.the74million.org/wp-content/uploads/2023/02/iStock-470493341-copy.jpg"
+          alt=""
+        />
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 Questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+/* CARD */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
