e99bded46565ba2f9de9a2aba5c2fe731fca1b37
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-20 11:31:20 +0900

    card component added and rendered from Card.vue file.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <script setup>
 import q from "./assets/data/quizes.json";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 
+// States
 const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
   <div class="container">
     <header>
       <h1>Quizzes</h1>
-      <input type="text" placeholder="Search..." />
+      <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizes" :key="quiz.id" />
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="image of quiz subject" />
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }}</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -52,30 +62,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-
-/* CARD */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <img
+      src="https://www.the74million.org/wp-content/uploads/2023/02/iStock-470493341-copy.jpg"
+      alt=""
+    />
+    <div class="card-text">
+      <h2>Math</h2>
+      <p>3 Questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
