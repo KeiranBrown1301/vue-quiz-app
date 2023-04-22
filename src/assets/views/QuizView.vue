957dc7cf58f457163b46ef777b84260aaccc2fab
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-22 10:25:15 +0900

    began the HTML for the quiz pages, aded some styles too.

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -1,5 +1,40 @@
+<script setup></script>
+
 <template>
   <div>
-    <h1>Hi!</h1>
+    <header>
+      <h4>Question 1/3</h4>
+      <div class="bar">
+        <div class="completion"></div>
+      </div>
+    </header>
+    <div>
+      <div class="question-container">
+        <h1 class="question">What is the chemical value of table salt?</h1>
+      </div>
+      <div class="questions-container"></div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+header {
+  margin-top: 20px;
+}
+
+header h4 {
+  font-size: 30px;
+}
+
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid bisque;
+}
+
+.completion {
+  height: 100%;
+  width: 0%;
+  background-color: bisque;
+}
+</style>
