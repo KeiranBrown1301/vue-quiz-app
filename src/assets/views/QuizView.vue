cc586d494deec709174a9b96adc3180a761550c3
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-22 12:11:00 +0900

    styling completed for the quiz page.

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -12,7 +12,14 @@
       <div class="question-container">
         <h1 class="question">What is the chemical value of table salt?</h1>
       </div>
-      <div class="questions-container"></div>
+      <div class="questions-container">
+        <div class="option">
+          <p class="option-label">A</p>
+          <div class="option-value">
+            <p>NaCl</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,5 +43,37 @@ header h4 {
   height: 100%;
   width: 0%;
   background-color: bisque;
+}
+
+.question-container {
+  margin-top: 20px;
+}
+
+.question {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+.option {
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.option-label {
+  background-color: bisque;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.option-value {
+  background-color: rgb(244, 239, 239);
+  width: 100%;
+  font-size: 30px;
+  padding: 0 20px;
 }
 </style>
