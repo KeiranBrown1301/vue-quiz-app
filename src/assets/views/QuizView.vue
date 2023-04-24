0366c8d686bb151913609374d08f8de00d41e328
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-24 19:11:22 +0900

    question component file created and filled with relevant code.

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import Question from "../../components/Question.vue";
+</script>
 
 <template>
   <div>
@@ -9,35 +11,7 @@
       </div>
     </header>
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the chemical value of table salt?</h1>
-      </div>
-      <div class="questions-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">B</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">C</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">D</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
+      <Question />
     </div>
   </div>
 </template>
@@ -61,37 +35,5 @@ header h4 {
   height: 100%;
   width: 0%;
   background-color: bisque;
-}
-
-.question-container {
-  margin-top: 20px;
-}
-
-.question {
-  font-size: 40px;
-  margin-bottom: 20px;
-}
-
-.option {
-  display: flex;
-  margin-bottom: 20px;
-  cursor: pointer;
-}
-
-.option-label {
-  background-color: bisque;
-  width: 50px;
-  height: 50px;
-  font-size: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.option-value {
-  background-color: rgb(244, 239, 239);
-  width: 100%;
-  font-size: 30px;
-  padding: 0 20px;
 }
 </style>
---
+++ src/components/Question.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+  <div class="question-container">
+    <h1 class="question">What is the chemical value of table salt?</h1>
+  </div>
+  <div class="questions-container">
+    <div class="option">
+      <p class="option-label">A</p>
+      <div class="option-value">
+        <p>NaCl</p>
+      </div>
+    </div>
+    <div class="option">
+      <p class="option-label">B</p>
+      <div class="option-value">
+        <p>NaCl</p>
+      </div>
+    </div>
+    <div class="option">
+      <p class="option-label">C</p>
+      <div class="option-value">
+        <p>NaCl</p>
+      </div>
+    </div>
+    <div class="option">
+      <p class="option-label">D</p>
+      <div class="option-value">
+        <p>NaCl</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.question-container {
+  margin-top: 20px;
+}
+
+.question {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+.option {
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.option-label {
+  background-color: bisque;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.option-value {
+  background-color: rgb(244, 239, 239);
+  width: 100%;
+  font-size: 30px;
+  padding: 0 20px;
+}
+</style>
