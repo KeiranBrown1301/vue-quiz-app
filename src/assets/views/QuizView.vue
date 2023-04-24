287b98be6cdbb9c9890e870fca5253c6aaa0b0ab
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-24 19:13:57 +0900

    did the same thing for the quiz header, both rendered in the Quiz View file.

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -1,39 +1,13 @@
 <script setup>
 import Question from "../../components/Question.vue";
+import QuizHeader from "../../components/QuizHeader.vue";
 </script>
 
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader />
     <div>
       <Question />
     </div>
   </div>
 </template>
-
-<style scoped>
-header {
-  margin-top: 20px;
-}
-
-header h4 {
-  font-size: 30px;
-}
-
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
-
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
-</style>
