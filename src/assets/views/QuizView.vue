b097c83b88e9571dba95d273329c6c2c7f194795
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-25 12:04:51 +0900

    results component created and props defined so that result is dynamically siaplayed

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -4,11 +4,14 @@ import QuizHeader from "../../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
 import { ref, watch, computed } from "vue";
 import quizzes from "../data/quizes.json";
+import Result from "../../components/Result.vue";
 
 const route = useRoute();
 const quizId = parseInt(route.params.id);
 const quiz = quizzes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
 
 // const questionStatus = ref(
 //   `${currentQuestionIndex.value}/${quiz.questions.length}`
@@ -28,6 +31,18 @@ const questionStatus = computed(
 const barPercentage = computed(
   () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
 );
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
 </script>
 
 <template>
@@ -37,8 +52,17 @@ const barPercentage = computed(
       :barPercentage="barPercentage"
     />
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]" />
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
-    <button @click="currentQuestionIndex++">Next Question</button>
+    <!-- <button @click="currentQuestionIndex++">Next Question</button> -->
   </div>
 </template>
