df53a0ac0a2577b78bd30577a26e475da1b236a6
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-25 10:48:47 +0900

    using computed implemented instead of the watch statement.

--- src/assets/views/QuizView.vue
+++ src/assets/views/QuizView.vue
@@ -2,7 +2,7 @@
 import Question from "../../components/Question.vue";
 import QuizHeader from "../../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import quizzes from "../data/quizes.json";
 
 const route = useRoute();
@@ -10,15 +10,20 @@ const quizId = parseInt(route.params.id);
 const quiz = quizzes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
 
-const questionStatus = ref(
-  `${currentQuestionIndex.value}/${quiz.questions.length}`
+// Here computed is used as the equivilant of what is belw with the questionStatus and watch use.
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
 );
-watch(
-  () => currentQuestionIndex.value,
-  () => {
-    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
-  }
-);
+
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//   }
+// );
 </script>
 
 <template>
